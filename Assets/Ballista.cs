8e5d42199dd358f01de9fcb32f53e206b95f1658
Matthias Rottländer <cliffhangerdangerautobot@gmail.com>
2021-01-29 01:32:48 +0100

    :construction: Add (maybe) ballistic projectile aiming

--- Assets/Ballista.cs
+++ Assets/Ballista.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ballista : MonoBehaviour
 {
     public Transform muzzle;
 
+    public float xAngle;
+    
     public void Update()
     {
         SearchForEnemy();
@@ -29,6 +29,20 @@ public class Ballista : MonoBehaviour
         {
             return;
         }
-        muzzle.LookAt(target.position);
+        Vector3 toTarget = target.transform.position - transform.position;
+        float y = -1;
+        toTarget.y = 0;
+        float x = toTarget.magnitude;
+        
+        
+        float v = 10;
+        float g = Physics.gravity.magnitude;
+        xAngle = Mathf.Atan((Mathf.Pow(v, 2) - Mathf.Sqrt(Mathf.Pow(v,4) - g*(g*Mathf.Pow(x,2) + 2*y*Mathf.Pow(v,2))) ) / g * x);
+
+        var quat1 = Quaternion.FromToRotation(Vector3.forward, toTarget.normalized);
+
+        Quaternion quaternion = quat1 * Quaternion.Euler(Mathf.Rad2Deg * xAngle, 0, 0);
+
+        muzzle.transform.rotation = quaternion;
     }
 }
