de854c471f8836f9cac8cc7b12aa351bc576202e
Matthias Rottländer <cliffhangerdangerautobot@gmail.com>
2021-01-30 00:27:39 +0100

    :sparkles: some working ballistics

--- Assets/Ballista.cs
+++ Assets/Ballista.cs
@@ -1,16 +1,18 @@
+using System.ComponentModel;
 using UnityEngine;
 
 public class Ballista : MonoBehaviour
 {
+    public Transform platform;
     public Transform muzzle;
 
     public float xAngle;
-    
+
     public void Update()
     {
         SearchForEnemy();
     }
-    
+
     private void SearchForEnemy()
     {
         Collider[] overlapSphere = Physics.OverlapSphere(transform.position, 5f);
@@ -29,20 +31,18 @@ public class Ballista : MonoBehaviour
         {
             return;
         }
-        Vector3 toTarget = target.transform.position - transform.position;
-        float y = -1;
-        toTarget.y = 0;
-        float x = toTarget.magnitude;
-        
-        
-        float v = 10;
-        float g = Physics.gravity.magnitude;
-        xAngle = Mathf.Atan((Mathf.Pow(v, 2) - Mathf.Sqrt(Mathf.Pow(v,4) - g*(g*Mathf.Pow(x,2) + 2*y*Mathf.Pow(v,2))) ) / g * x);
+        Vector3 toTarget = target.position - transform.position;
+        float offsetVertical = toTarget.y;
+        Vector3 offsetHorizontal = toTarget;
+        offsetHorizontal.y = 0;
+        float g = Physics.gravity.y;
+        float h = 1.5f;
+        Vector3 u_right = offsetHorizontal / (Mathf.Sqrt(-2 * h / g) + Mathf.Sqrt(2 * (offsetVertical - h) / g));
+        float u_up = Mathf.Sqrt(-2 * g * h);
 
-        var quat1 = Quaternion.FromToRotation(Vector3.forward, toTarget.normalized);
-
-        Quaternion quaternion = quat1 * Quaternion.Euler(Mathf.Rad2Deg * xAngle, 0, 0);
-
-        muzzle.transform.rotation = quaternion;
+        var vHorizontal = u_right;
+        var vVertical = Vector3.up * u_up;
+        Debug.Log(vHorizontal + vVertical);
+        muzzle.transform.LookAt(muzzle.position + (vHorizontal + vVertical), Vector3.up);
     }
 }
