8f5060b1b6852815533b16d58286ee854844abb7
Matthias Rottländer <cliffhangerdangerautobot@gmail.com>
2021-02-07 18:31:09 +0100

    :sparkles: Have zoomable Lane View

--- Assets/Editor/WaveDataEditorWindow.cs
+++ Assets/Editor/WaveDataEditorWindow.cs
@@ -14,6 +14,7 @@ public class WaveDataEditorWindow : EditorWindow
             alignment = TextAnchor.MiddleCenter,
             overflow = new RectOffset()
         };
+
         public static GUIStyle ruler = new GUIStyle("box")
         {
             fixedHeight = 20,
@@ -27,12 +28,20 @@ public class WaveDataEditorWindow : EditorWindow
             {
                 background = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Editor/Background_Spawn.png")
             },
-            overflow = new RectOffset(-5,-5,-5,-5),
-            padding = new RectOffset(5,5,5,5),
+            border = new RectOffset(10, 10, 10, 10),
+            overflow = new RectOffset(0, 0, -5, -5),
+            padding = new RectOffset(5, 5, 5, 5),
             alignment = TextAnchor.MiddleCenter
         };
     }
+
+    public class WaveViewSettings
+    {
+        public bool Snapping { get; set; } = true;
+        public float PixelPerSecond { get; set; } = 15;
+    } 
     
+    public WaveViewSettings settings = new WaveViewSettings();
     public WaveData waveData;
     private bool _toggle;
     private Vector2 _scrollPosition;
@@ -65,9 +74,10 @@ public class WaveDataEditorWindow : EditorWindow
     private void DrawToolbar()
     {
         EditorGUILayout.BeginHorizontal(EditorStyles.toolbar);
-        _toggle = EditorGUILayout.Toggle(EditorGUIUtility.TrIconContent("GridAxisX", "Snap to X"),
-            _toggle,
+        settings.Snapping = GUILayout.Toggle(settings.Snapping, EditorGUIUtility.TrIconContent("GridAxisX", "Snap to X"),
             EditorStyles.toolbarButton);
+        settings.PixelPerSecond = EditorGUILayout.Slider("a", settings.PixelPerSecond, 0.2f, 30f);
+        GUILayout.FlexibleSpace();
         EditorGUILayout.EndHorizontal();
     }
 
@@ -82,6 +92,7 @@ public class WaveDataEditorWindow : EditorWindow
     {
         EditorGUILayout.BeginVertical(new GUIStyle(),GUILayout.Width(80));
         EditorGUILayout.LabelField(new GUIContent("ruler"), Styles.ruler, GUILayout.ExpandWidth(true));
+        EditorGUILayout.Space();
         foreach (LaneSO waveDataLane in waveData.lanes)
         {
             EditorGUILayout.LabelField(new GUIContent(waveDataLane.name), Styles.lane, GUILayout.ExpandWidth(true));
@@ -92,7 +103,9 @@ public class WaveDataEditorWindow : EditorWindow
     {
         _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
         EditorGUILayout.BeginVertical(new GUIStyle(), GUILayout.Width(1200));
-        EditorGUILayout.LabelField("ruler", Styles.ruler, GUILayout.ExpandWidth(true));
+        Rect rulerRect = EditorGUILayout.GetControlRect(false, Styles.ruler.fixedHeight, Styles.ruler, GUILayout.ExpandWidth(true));
+        DrawRuler(rulerRect);
+        EditorGUILayout.Space();
         foreach (LaneSO waveDataLane in waveData.lanes)
         {
             Rect controlRect = EditorGUILayout.GetControlRect(false, 60, Styles.lane, GUILayout.ExpandWidth(true));
@@ -103,29 +116,40 @@ public class WaveDataEditorWindow : EditorWindow
     }
     private void DrawLaneBody(LaneSO lane, Rect controlRect)
     {
-        Handles.color = new Color(.35f,.35f,.35f);
+        DrawRuler(controlRect);
+        DrawLaneContent(controlRect);
+    }
+    private static void DrawRuler(Rect controlRect)
+    {
+        int pixelsPerSecond = 15;
+        Handles.color = new Color(.35f, .35f, .35f);
         float mark = 0;
-        EditorGUI.DrawRect(controlRect, new Color(.20f,.20f,.20f));
+        EditorGUI.DrawRect(controlRect, new Color(.20f, .20f, .20f));
         while (mark < controlRect.width)
         {
             var start = new Vector3(controlRect.x + mark, controlRect.y, 0);
             var end = new Vector3(controlRect.x + mark, controlRect.y + controlRect.height, 0);
             Handles.DrawLine(start, end);
-            mark += 10;
+            mark += 5 * pixelsPerSecond;
         }
-
-        DrawLaneContent(lane, controlRect);
     }
-    private void DrawLaneContent(LaneSO lane, Rect controlRect)
+    private void DrawLaneContent(Rect controlRect)
     {
         List<Spawn> spawns = waveData.spawns;
         for (var i = 0; i < spawns.Count; i++)
         {
             Spawn spawn = spawns[i];
-            EditorGUI.LabelField(new Rect(controlRect) { x = controlRect.x + spawn.StartTime, width = spawn.EndTime - spawn.StartTime}, "box", Styles.spawn);
+            DrawSpawn(controlRect, spawn, settings);
         }
     }
-    private void SpawnsForLane(LaneSO lane)
+    private static void DrawSpawn(Rect controlRect, Spawn spawn, WaveViewSettings waveViewSettings)
     {
+        float startPixel = (controlRect.x + spawn.StartTime) * waveViewSettings.PixelPerSecond;
+        float widthPixel = (spawn.EndTime - spawn.StartTime) * waveViewSettings.PixelPerSecond;
+        var spawnRect = new Rect(controlRect) {x = startPixel, width = widthPixel};
+        EditorGUI.LabelField(spawnRect,
+            "box",
+            Styles.spawn);
+        EditorGUIUtility.AddCursorRect(spawnRect, MouseCursor.Pan);
     }
 }
