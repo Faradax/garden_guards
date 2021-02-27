b87873991e1269c3595340775667d75af1963bb1
Matthias Rottländer <cliffhangerdangerautobot@gmail.com>
2021-02-27 22:29:53 +0100

    :sparkles: Add Lightning Effect

--- Assets/Coin/Rotate.cs
+++ Assets/Coin/Rotate.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class Rotate : MonoBehaviour
 {
+    public int rotPerSecond = 60;
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        transform.Rotate(Vector3.up, 1);
+        transform.Rotate(Vector3.up, rotPerSecond * Time.deltaTime);
     }
 }
