e155268cda7b9c9c466e7d786688c43266663594
Matthias Rottländer <cliffhangerdangerautobot@gmail.com>
2021-05-16 18:26:54 +0200

    :sparkles: Show Countdown to Wave

--- Assets/HitPointDisplayController.cs
+++ Assets/HitPointDisplayController.cs
@@ -8,18 +8,33 @@ public class HitPointDisplayController : MonoBehaviour
     public UIDocument document;
     
     public Counter hitPointCounter;
+    public Countdown preparationCountdown;
     private Label _hitPointLabel;
     
+    private VisualElement _preparationCountdown;
+    private Label _remainingTimeLabel;
+
     private void OnEnable()
     {
         _hitPointLabel = document.rootVisualElement.Q<Label>("hitPoints");
+        _remainingTimeLabel = document.rootVisualElement.Q<Label>("remainingTime");
+        _preparationCountdown = document.rootVisualElement.Q<VisualElement>("preparationCountdown");
+        
         hitPointCounter.ValueChanged += UpdateHitPoints;
+        preparationCountdown.valueChanged.AddListener(UpdateCountdown);
+        
         UpdateHitPoints(hitPointCounter.Value);
+    }
+    private void UpdateCountdown(int newValue)
+    {
+        _remainingTimeLabel.text = newValue.ToString();
+        _preparationCountdown.style.display = newValue == 0 ? DisplayStyle.None : DisplayStyle.Flex;
     }
 
     private void OnDisable()
     {
         hitPointCounter.ValueChanged -= UpdateHitPoints;
+        preparationCountdown.valueChanged.RemoveListener(UpdateCountdown);
     }
     private void UpdateHitPoints(int newAmount)
     {
